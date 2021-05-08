2e81429d2999b4c649ba03c55e9cbfa187e23440
Кристиян Стоянов <82226409+heliumbg@users.noreply.github.com>
2021-05-08 16:43:44 +0300

    add new

--- assets/js/google-maps.js
+++ assets/js/google-maps.js
@@ -1,15 +1,15 @@
 function initMap() {
     // Latitude and Longitude
-    var myLatLng = {lat: 43.571561589087466, lng: 27.802420255603707};
+    var myLatLng = {lat: 43.2124851, lng: 27.874877};
 
     var map = new google.maps.Map(document.getElementById('myMap'), {
-        zoom: 17,
+        zoom: 12,
         center: myLatLng
     });
 
-    var marker = new google.maps.Marker({
+    /*var marker = new google.maps.Marker({
         position: myLatLng,
         map: map,
-        title: 'South Jakarta, INA' // Title Location
-    });
+        title: 'Varna' // Title Location
+    });*/
 }
